c37a46f95ab820a50ec103404ddc97c72b33cdf7
Zibo Zhang <tonyzhang95@gmail.com>
2020-04-22 12:56:24 -0400

    updated sign up page
    sign up pages now can fetch parameters from UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 
@@ -7,14 +7,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():
+@app.route('/showLogIn')
+def showLogIn():
     return render_template('login.html')
 
 
-@app.route('/signup')
-def signup():
+@app.route('/LogIn')
+def logIn():
+    return 'Log in'
+
+
+@app.route('/showSignUp')
+def showSignUp():
     return render_template('signup.html')
+
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 
 @app.route('/todos')
@@ -29,4 +49,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000')
