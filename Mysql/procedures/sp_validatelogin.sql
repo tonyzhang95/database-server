ba2f768bd4d5edfd8b3f9b5293e65d8e3356d9ff
jadewu <jadewunue@gmail.com>
2020-04-25 15:38:20 -0400

    Create sp_validatelogin.sql

---
+++ Mysql/procedures/sp_validatelogin.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validatelogin`(
+ IN p_username VARCHAR(45)
+    )
+BEGIN
+ SELECT * FROM WDS.user WHERE user_username=p_username;
+END$$
+DELIMITER ;
