faae8048735f7db39c9555732aa5a5612cbcf893
Zibo Zhang <tonyzhang95@gmail.com>
2020-04-25 00:34:06 -0400

    created user information page
    User info page needs debug.
    Now after user sign up, they will be redirected to user informatio page where they can update thier information. Signup will also log in automatically.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, json, redirect, session
+from flask import Flask, render_template, request, json, redirect, session, url_for
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -65,33 +66,39 @@ def showSignUp():
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
     try:
+        print(request.url)
+        print(request.args)
+
         _name = request.form['inputName']
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
 
-        # validate the received values
         if _name and _email and _password:
 
-            # All Good, let's call MySQL
-            print('------sign up info received')
             conn = mysql.connect()
-            print('------connection established')
             cursor = conn.cursor()
-            print('------cursor created')
             _hashed_password = generate_password_hash(_password)
             cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
             data = cursor.fetchall()
 
             if len(data) is 0:
                 conn.commit()
-                print('------created user: ',_name,_email,_password)
                 cursor.close()
                 conn.close()
-                return json.dumps({'message':'User created!'})
+
+                session['user'] = _email
+
+                if '@' in _email:
+                    if _email.split('@')[1] == 'wds.com':
+                        return json.dumps({'response': "employee"}) # redirects to employee home
+                    else:
+                        return json.dumps({'response': "user"})
+                else:
+                    return json.dumps({'response': "user"}) # redirects to user info page.
             else:
-                return json.dumps({'message':'Email existed, please sign in or try with another email.'})
+                return json.dumps({'response':'Email existed, please sign in or try with another email.'})
         else:
-            return json.dumps({'message':'Enter the required fields'})
+            return json.dumps({'response':'Enter the required fields'})
 
     except Exception as e:
         return json.dumps({'error':str(e)})
@@ -107,30 +114,91 @@ def userHome():
         sql = 'SELECT * FROM WDS.user WHERE user_username = {}'.format('"' + str(session.get('user')) + '"')
         cursor.execute(sql)
         result = cursor.fetchall()
+        print(result[0][:-1])
+        user_account = 'User name: ' + result[0][1] + "          Accunt/Email: " + result[0][2]
 
-        return render_template('userHome.html', user_info = str(result), user_insurance = 'car insurance 001')
+        return render_template('userHome.html', user_info = user_account, user_insurance = 'car insurance 001')
     else:
-        return render_template('error.html', error = 'Unauthorized User')
+        return render_template('error.html', error = 'Unauthorized Access')
 
 
+"""
+Process submitted user info form.
+Pass in parameters with RESTful query POST request.
+For example:
+http://0.0.0.0:5000/processUserInfo?firstname=david&lastname=beckham&gender=M&maritality=S&instype=A&house=500&street=Man St&city=manchester&state=NY&zipcode=10003
+"""
+@app.route('/processUserInfo', methods = ['POST'])
+def processUserInfo():
+    try:
+        print("------", request.url)
+        firstname = request.form['cFirstname']
+
+        lastname = request.form['cLastname']
+        print('++++++')
+        gender = request.form['cGender']
+        print('++++++')
+        maritality = request.form['cMaritality']
+        instype = request.form['cInstype']
+        house = request.form['cHouse']
+        street = request.form['cStreet']
+        city = request.form['cCity']
+        state = request.form['cState']
+        zipcode = request.form['cZipcode']
+
+
+        print(firstname, lastname, gender, maritality, instype, house, street, city, state, zipcode)
+
+
+        if firstname and lastname and gender and maritality and instype and house and street and city and state and zipcode: # check all fields filled
+
+            # conn = mysql.connect()
+            # cursor = conn.cursor()
+            # sql = 'INSERT INTO WDS.customer (cfirstname, clastname, cgender, cmaritality, cinstype, chouse, cstreet, ccity, cstate, czipcode) VALUES ("' + '","'.join([firstname, lastname, gender, maritality, instype, house, street, city, state, zipcode]) + '"'
+            # cursor.execute(sql)
+            # data = cursor.fetchall()
+
+            if len(data) == 0:
+                return "Successfully entered this record to the database." + " ".join([firstname, lastname, gender, maritality, instype, house, street, city, state, zipcode])
+        else:
+            return "Error: must fill all fields."
+    except Exception as e:
+        print(str(e))
+        return str(e)
+
+
+# interface for user info edit
+@app.route('/userInfo')
+def userInfo():
+    if session.get('user'):
+        return render_template('userinfo.html')
+    else:
+        return render_template('error.html', error = 'You need to log-in to access this page.')
+
+
+# display products and pricing
 @app.route('/product')
 def product():
     return render_template('product.html')
 
 
+# log out current user
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return redirect('/')
 
 
+# temporary page for todos
 @app.route('/todos')
 def todo():
     return render_template('todos.html')
 
 
+# documentation page for this project
 @app.route('/about')
 def about():
+    # return redirect('/userInfo')
     return render_template('about.html')
 
 
--- static/js/userInfo.js
+++ static/js/userInfo.js
@@ -1,14 +1,13 @@
 $(function() {
-    $('#btnSignUp').click(function() {
+    $('#btnSave').click(function() {
         $.ajax({
-            url: '/signUp',
+            url: '/processUserInfo',
             data: $('form').serialize(),
             type: 'POST',
             dataType: 'json',
             success: function (result, status, xhr) {
                 console.log(result);
-                $("#result").html(result.response);
-                if (result.response == "user") window.location.href='/userInfo'
+
                 },
             error: function (xhr, status, error) {
                 $("#result").html("Error: " + xhr.responseText)
